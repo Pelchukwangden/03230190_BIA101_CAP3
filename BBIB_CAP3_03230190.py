5d4c08b5f90fad9df7ffd5acd87a4b8af2841aea
Pelchukwangden <03230190.gcbs@rub.edu.bt>
2024-05-25 18:59:27 +0600

    first commit

---
+++ BBIB_CAP3_03230190.py
@@ -0,0 +1,13 @@
+################################
+# Github Repo link: https://github.com/your_username/YourStudentNumber_BIA101_CAP3
+# Your Name: Pelden Wangchuk
+# Your Section: B
+# Your Student ID Number: 03230190
+################################
+# REFERENCES
+# No external references used
+################################
+# SOLUTION
+# Your Solution Score: <total sum>
+# ################################
+
